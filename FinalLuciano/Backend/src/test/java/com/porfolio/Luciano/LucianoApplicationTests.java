e2bee8d758fde97de731c26c4b6219f4afb2aa5c
6luciano3 <99690369+6luciano3@users.noreply.github.com>
2022-08-17 06:05:10 -0300

    creamos el backend

---
+++ FinalLuciano/Backend/src/test/java/com/porfolio/Luciano/LucianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.Luciano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
